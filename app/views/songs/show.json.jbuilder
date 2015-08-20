60ac2640b99da3ce4cf16df2b72f3d898a9b01bf
Brant Padak <brantpadak@yahoo.com>
2015-08-20 13:33:28 -0400

    first commit

---
+++ app/views/songs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @song, :id, :name, :rating, :artist_id, :album, :created_at, :updated_at
---
+++ db/migrate/20150820122552_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :rating
+      t.integer :artist_id
+      t.string :album
+
+      t.timestamps null: false
+    end
+  end
+end
